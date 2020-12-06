cebd5612c062f7a4ff7daff8d950042bc7f52094
Ashish Maharjan <amaharjan1032@gmail.com>
2020-12-06 10:03:34 +0545

    Client setup and reactstrap done

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+} from "reactstrap";
+
+const AppNavbar = (props) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => setIsOpen(!isOpen);
+
+	return (
+		<div>
+			<Navbar color="dark" light expand="md" dark>
+				<NavbarBrand href="/">Shopping List</NavbarBrand>
+				<NavbarToggler onClick={toggle} />
+				<Collapse isOpen={isOpen} navbar>
+					<Nav className="ml-auto" navbar>
+						<NavItem>
+							<NavLink href="https://github.com/AshishMhrzn10/MERN-todo-traversy-">
+								GitHub
+							</NavLink>
+						</NavItem>
+					</Nav>
+				</Collapse>
+			</Navbar>
+		</div>
+	);
+};
+
+export default AppNavbar;
